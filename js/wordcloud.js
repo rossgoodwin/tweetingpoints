a90a8f8fd1b23ac213a7e03db2ee49e4fb6d7df9
Ross Goodwin <ross.goodwin@gmail.com>
2015-07-24 14:39:47 -0400

    transitions

--- js/wordcloud.js
+++ js/wordcloud.js
@@ -2,7 +2,7 @@
 
 $.getJSON("http://54.148.103.250/", function(candidateTweets){
 
-  $("#loadinggif").hide();
+  $("#loadinggif").fadeOut("slow");
 
   function fill1(x) {
     var blues = ["#3498DB", "#59ABE3", "#4183D7", "#4B77BE", "#19B5FE", "#3A539B", "#89C4F4", "#446CB3"];
@@ -33,7 +33,8 @@ $.getJSON("http://54.148.103.250/", function(candidateTweets){
   function draw(words) {
     var overallContainer = d3.select("#vis")
                             .append("div")
-                            .attr("class", "col-lg-6 col-md-12 col-sm-12 col-xs-12");
+                            .attr("class", "col-lg-6 col-md-12 col-sm-12 col-xs-12 transparent")
+                            .style("opacity", 0.0);
 
     var svgContainer = overallContainer.append("svg")
         .attr("width", layout.size()[0])
@@ -65,6 +66,11 @@ $.getJSON("http://54.148.103.250/", function(candidateTweets){
                     .attr("href", "https://twitter.com/"+candidateTweets[n]['name'])
                     .attr("class", "twitter-link")
                     .text('@'+candidateTweets[n]['name']);
+
+    overallContainer.transition()
+        .style("opacity", 1.0)
+        .delay(3000)
+        .duration(3000);
   }
 
 });
